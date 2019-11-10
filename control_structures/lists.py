8adcf2f17b5ceba51d90afe355e3ad951f2abaa3
Gigi Minova <minova.gigi@gmail.com>
2019-11-10 23:38:10 +0000

    lists

---
+++ control_structures/lists.py
@@ -0,0 +1,12 @@
+fruits = ['apple', 'pear', 'peach']
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+
+print([])
+number = 3
+mixed_list = ['string', 1, ['apple', 1, number], 4.56, fruits[1]]
+print(mixed_list[4])
+
+some_string = 'abcdefg'
+print(some_string[0])
