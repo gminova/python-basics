011146badf29d6539f899f7c0092361b2ecd5cdd
Gigi Minova <minova.gigi@gmail.com>
2019-11-10 20:24:31 +0000

    variables

---
+++ variables.py
@@ -0,0 +1,12 @@
+# assign a variable
+new_variable = 'some random string'
+# reference a variable
+print(new_variable)
+spam = 'spam'
+breakfast = spam + ' and eggs'
+print(breakfast)
+# delete a variable
+del breakfast
+spam = 123
+# reassign a variable
+print(spam)
