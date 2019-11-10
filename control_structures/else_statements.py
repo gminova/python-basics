68bc2b448d8cc42f1fb8c4e2839216e305f9577b
Gigi Minova <minova.gigi@gmail.com>
2019-11-10 22:27:10 +0000

    else and elif statements

---
+++ control_structures/else_statements.py
@@ -0,0 +1,29 @@
+num = 4
+
+if num == 5:
+    print('num is equal to 5')
+else:
+    print('num is not equal to 5')
+
+age = 10
+
+if age <= 12:
+    print('child')
+else:
+    if age <= 59:
+        print('adult')
+    else:
+        print('elderly')
+
+grade = 66
+
+if grade < 3:
+    print('Fail')
+elif grade < 3.5:
+    print('Poor')
+elif grade < 4.5:
+    print('Good')
+elif grade < 5.5:
+    print('Very good')
+else:
+    print('Excellent')
