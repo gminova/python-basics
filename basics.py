ef667931c3191c92ab83d3b31f1972172afdbeba
Gigi Minova <minova.gigi@gmail.com>
2019-11-10 17:46:02 +0000

    printing stuff

---
+++ basics.py
@@ -0,0 +1,17 @@
+# Print
+print('Hello, Python')
+print('Print', 'Multiple', 'Arguments')
+print('First line \nSecond line')
+
+# Maths
+print('5 + 7 = ', 5 + 7)
+print('6 - 2 = ', 6 - 2)
+print('8 * 4 = ', 8 * 4)
+print('9 / 3 = ', 9 / 3)
+print('2 ** 2 = ', 2 ** 2)
+print('5 % 6 = ', 5 % 6)
+print('5 % 5 = ', 5 % 5)
+print('5 + 5 * 5 = ', 5 + 5 * 5)
+print('(10 - 2) * 4 = ', (10 - 2) * 4)
+print('10 / 3 = ', 10 / 3)
+print('1.000000000000001 * 8 = ', 1.000000000000001 * 8)
