1302a4c7da16656aaac89451b0593fcfea303788
Gigi Minova <minova.gigi@gmail.com>
2019-11-10 21:43:04 +0000

    booleans and comparisons

---
+++ control_structures/booleans_and_comparisons.py
@@ -0,0 +1,9 @@
+my_boolean = True
+print(my_boolean, ' must be True')
+print(2 == 3, ' must be False')
+print('2' == 2, ' must be False')
+print('hello' == 'hello', ' must be True')
+print('hello' != 'goodbye', ' must be True')
+print(10.0 > 10, ' must be False')
+print(4 > 5, ' must be False')
+print(4 >= 4, ' must be True')
