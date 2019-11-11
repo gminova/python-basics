45f946f131f6c3ed1df53af62c8449315e82cd83
Gigi Minova <minova.gigi@gmail.com>
2019-11-11 01:01:17 +0000

    function arguments

---
+++ functions_and_modules/function_arguments.py
@@ -0,0 +1,30 @@
+def greet_me(name):
+    print('Hello, ' + name)
+
+
+greet_me('Gigi')
+
+
+def print_double(x):
+    print(2 * x)
+
+
+print_double(3)
+
+
+def print_sum_twice(x, y):
+    print(x + y)
+    print(x + y)
+
+
+print_sum_twice(5, 8)
+
+
+def even(x):
+    if x % 2 == 0:
+        print("Yes")
+    else:
+        print("No")
+
+
+even(4)
