7b5b31256883696e6395d5e33fb39eef9347c56b
Gigi Minova <minova.gigi@gmail.com>
2019-11-17 22:12:01 +0000

    comments and docstrings

---
+++ functions_and_modules/comments_and_docstrings.py
@@ -0,0 +1,21 @@
+# one line comment
+"""
+Docstrings (documentation strings) serve
+a similar purpose to comments, as they are 
+designed to explain code. However, they are
+more specific and have a different syntax. 
+They are created by putting a multiline string 
+containing an explanation of the function below 
+the function's first line.
+"""
+
+
+def shout(word):
+    """
+    Print a word with an
+    exclamation mark following it.
+    """
+    print(word + "!")
+
+
+shout("spam")
