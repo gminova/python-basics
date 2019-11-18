63cb8d3d0087900145461b1fca32ace6f8d00e68
Gigi Minova <minova.gigi@gmail.com>
2019-11-18 01:28:44 +0000

    modules

---
+++ functions_and_modules/modules.py
@@ -0,0 +1,15 @@
+import random
+import math
+from math import pi, sqrt
+from math import sqrt as square_root
+# from math import *
+
+for i in range(5):
+    value = random.randint(1, 6)
+    print(value)
+
+num = 10
+
+print(math.sqrt(num))
+print(pi)
+print(square_root(25))
