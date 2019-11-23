34b2ad3c4a47a1d000186c51ab826fdf7b8fe4be
Gigi Minova <minova.gigi@gmail.com>
2019-11-23 18:25:46 +0000

    exceptions

---
+++ exceptions_and_files/exceptions.py
@@ -0,0 +1,11 @@
+# Exceptions
+
+# Different exceptions are raised for different reasons.
+# Common exceptions:
+# ImportError: an import fails
+# IndexError: a list is indexed with an out-of-range number
+# NameError: an unknown variable is used
+# SyntaxError: the code can't be parsed properly
+# TypeError: a function is called on a value of an inappropriate type
+# ValueError: a function is called on a value of the correct type, but with an inappropriate value.
+# Python has several other built-in exceptions, such as ZeroDivisionError and OSError. Third-party libraries also often define their own exceptions.
