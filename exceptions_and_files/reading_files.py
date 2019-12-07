3f69206277f89a0ca73f553055244f516447c5a3
Gigi Minova <minova.gigi@gmail.com>
2019-12-07 23:53:03 +0000

    read files

--- exceptions_and_files/reading_files.py
+++ exceptions_and_files/reading_files.py
@@ -2,3 +2,9 @@ file = open('exceptions_and_files/myfile.txt')
 content = file.read()
 print(content)
 file.close()
+
+file = open('exceptions_and_files/myfile.txt')
+# print(file.readlines())
+for line in file:
+    print(line)
+file.close()
