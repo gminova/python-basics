242fd8d54ab35ee022e4b3d20a699a08d47aedd6
Gigi Minova <minova.gigi@gmail.com>
2019-12-07 23:35:33 +0000

    read files

--- exceptions_and_files/opening_files.py
+++ exceptions_and_files/opening_files.py
@@ -1,12 +1,12 @@
 file = open('exceptions_and_files/myfile.txt')
 print(file.read(15))  # print first 15 chars
 
-# write mode
-open("exceptions_and_files/myfile.txt", "w")
+# # write mode
+# open("exceptions_and_files/myfile.txt", "w")
 
-# read mode (default mode)
-open("exceptions_and_files/myfile.txt", "r")
-open("exceptions_and_files/myfile.txt")
+# # read mode (default mode)
+# open("exceptions_and_files/myfile.txt", "r")
+# open("exceptions_and_files/myfile.txt")
 
-# binary write mode
-open("exceptions_and_files/myfile.txt", "wb")
+# # binary write mode
+# open("exceptions_and_files/myfile.txt", "wb")
