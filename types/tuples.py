667107ff0219267b828fa0b988c32178b8080a78
Gigi Minova <minova.gigi@gmail.com>
2019-12-08 20:53:13 +0000

    tuples

---
+++ types/tuples.py
@@ -0,0 +1,7 @@
+# Tuples are very similar to lists, except that they are immutable(they cannot be changed).
+# Also, they are created using parentheses, rather than square brackets.
+words = ('spam', 'eggs', 'ham',)
+nums = 1, 2, 3
+print(words[0])
+print(words[2])
+print(nums[1])
