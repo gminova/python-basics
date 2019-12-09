52b4dfffae55c34d7f0721c01a6a2790d4d67c79
Gigi Minova <minova.gigi@gmail.com>
2019-12-09 07:23:31 +0000

    list comprehensions

--- types/list_comprehension.py
+++ types/list_comprehension.py
@@ -2,3 +2,13 @@
 cubes = [i**3 for i in range(5)]
 
 print(cubes)  # [0, 1, 8, 27, 64]
+
+nums = [i*2 for i in range(10)]
+
+print(nums)
+
+evens = [i**2 for i in range(10) if i**2 % 2 == 0]
+
+print(evens)
+
+even = [2*i for i in range(10**100)]  # MemoryError
