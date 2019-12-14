3d6fcab59f933f95708b4b64398866c9fda6839d
Gigi Minova <minova.gigi@gmail.com>
2019-12-14 15:52:50 +0000

    usefult functions

--- types/useful_functions.py
+++ types/useful_functions.py
@@ -18,3 +18,14 @@ print(max([1, 4, 9, 2, 5, 6, 8]))
 print(abs(-99))
 print(abs(42))
 print(sum([1, 2, 3, 4, 5]))
+
+nums = [55, 44, 33, 22, 11]
+
+if all([i > 5 for i in nums]):
+    print("All larger than 5")
+
+if any([i % 2 == 0 for i in nums]):
+    print("At least one is even")
+
+for v in enumerate(nums):
+    print(v)
