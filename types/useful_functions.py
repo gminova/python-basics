aaca238d6a72e6bddb8df12cc543b56436095efd
Gigi Minova <minova.gigi@gmail.com>
2019-12-14 15:50:25 +0000

    usefult functions

--- types/useful_functions.py
+++ types/useful_functions.py
@@ -11,3 +11,10 @@ print("This is a sentence.".upper())
 print("AN ALL CAPS SENTENCE".lower())
 
 print("spam, eggs, ham".split(", "))
+
+
+print(min(1, 2, 3, 4, 0, 2, 1))
+print(max([1, 4, 9, 2, 5, 6, 8]))
+print(abs(-99))
+print(abs(42))
+print(sum([1, 2, 3, 4, 5]))
