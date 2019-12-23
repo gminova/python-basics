be96c6aa1c27cbf57c7ff2ddd6de1ffba9f14d16
Gigi Minova <minova.gigi@gmail.com>
2019-12-23 08:57:34 +0000

    lambdas

--- functional_programming/lambdas.py
+++ functional_programming/lambdas.py
@@ -3,3 +3,15 @@ def my_func(f, arg):
 
 
 print(my_func(lambda x: 2*x*x, 5))
+
+# named function
+
+
+def polynomial(x):
+    return x**2 + 5*x + 4
+
+
+print(polynomial(-4))
+
+# lambda
+print((lambda x: x**2 + 5*x + 4)(-4))
