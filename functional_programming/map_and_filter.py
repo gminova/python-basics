0350d1ddfe1e567d799a8d7c2fe44351b27a5876
Gigi Minova <minova.gigi@gmail.com>
2019-12-24 13:03:52 +0000

    filter

--- functional_programming/map_and_filter.py
+++ functional_programming/map_and_filter.py
@@ -5,3 +5,12 @@ def add_five(x):
 nums = [1, 2, 3]
 result = list(map(add_five, nums))
 print(result)
+
+
+nums = [11, 22, 33, 44, 55]
+res = list(filter(lambda x: x % 2 == 0, nums))
+print(res)
+
+nums = [1, 2, 5, 8, 3, 0, 7]
+res = list(filter(lambda x: x < 5, nums))
+print(res)
