c80a1ae21e09a60e71e4d87bd9059decebf43270
Gigi Minova <minova.gigi@gmail.com>
2019-12-25 18:40:17 +0000

    sets

--- functional_programming/sets.py
+++ functional_programming/sets.py
@@ -2,3 +2,23 @@ num_set = {1, 2, 3}
 word_set = set(['spam', 'eggs', 'tomatoes'])
 print(3 in num_set)
 print('eggs' not in word_set)
+
+nums = {1, 2, 1, 3, 1, 4, 5, 6}
+print(nums)
+nums.add(-7)
+nums.remove(3)
+print(nums)
+
+# Sets can be combined using mathematical operations.
+# The union operator | combines two sets to form a new one containing items in either.
+# The intersection operator & gets items only in both.
+# The difference operator - gets items in the first set but not in the second.
+# The symmetric difference operator ^ gets items in either set, but not both.
+first = {1, 2, 3, 4, 5, 6}
+second = {4, 5, 6, 7, 8, 9}
+
+print(first | second)
+print(first & second)
+print(first - second)
+print(second - first)
+print(first ^ second)
