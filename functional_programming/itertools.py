3b0d48e5964bca0f41760a60fe73815777770b8d
Gigi Minova <minova.gigi@gmail.com>
2019-12-25 19:18:34 +0000

    itertools

--- functional_programming/itertools.py
+++ functional_programming/itertools.py
@@ -1,6 +1,10 @@
-from itertools import count
+from itertools import count, accumulate, takewhile
 
 for i in count(3):
     print(i)
     if i >= 11:
         break
+
+nums = list(accumulate(range(8)))
+print(nums)
+print(list(takewhile(lambda x: x <= 6, nums)))
