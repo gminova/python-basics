d8d5b2e72c41e3c1e104932e27dac8e474200ec9
Gigi Minova <minova.gigi@gmail.com>
2019-12-25 17:52:21 +0000

    add decorators

--- functional_programming/generators.py
+++ functional_programming/generators.py
@@ -8,9 +8,11 @@ def countdown():
 for i in countdown():
     print(i)
 
+
 def numbers(x):
     for i in range(x):
-        if i%2 == 0:
+        if i % 2 == 0:
             yield i
+
 
 print(list(numbers(11)))
