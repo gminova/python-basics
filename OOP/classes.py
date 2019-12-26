7308a4da535a1fa255a3ef1f737596fc21f16a3d
Gigi Minova <minova.gigi@gmail.com>
2019-12-26 13:47:19 +0000

    classes

--- OOP/classes.py
+++ OOP/classes.py
@@ -6,3 +6,30 @@ class Cat:
 
 felix = Cat("ginger", 4)
 print(felix.color)
+
+
+class Dog:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def bark(self):
+        print("Woof!")
+
+
+fido = Dog("Fido", "brown")
+print(fido.name)
+fido.bark()
+
+
+class Doggo:
+    legs = 4
+
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+
+fido = Doggo("Fido", "brown")
+print(fido.legs)
+print(Doggo.legs)
