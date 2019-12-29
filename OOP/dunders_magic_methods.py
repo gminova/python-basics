a2387b6cb73cf1807aee8823d0d189d8fde84dff
Gigi Minova <minova.gigi@gmail.com>
2019-12-29 13:38:36 +0000

    special string

--- OOP/dunders_magic_methods.py
+++ OOP/dunders_magic_methods.py
@@ -23,3 +23,17 @@ print(result.y)
 # __and__ for &
 # __xor__ for ^
 # __or__ for |
+
+
+class SpecialString:
+    def __init__(self, cont):
+        self.cont = cont
+
+    def __truediv__(self, other):
+        line = "=" * len(other.cont)
+        return "\n".join([self.cont, line, other.cont])
+
+
+spam = SpecialString("spam")
+hello = SpecialString("Hello world!")
+print(spam / hello)
