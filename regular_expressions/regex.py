e3b093f7c8d437c7f245d359ea063d1325285e55
Gigi Minova <minova.gigi@gmail.com>
2020-01-03 22:32:46 +0000

    regex

--- regular_expressions/regex.py
+++ regular_expressions/regex.py
@@ -6,3 +6,17 @@ if re.match(pattern, 'spamspamspam'):
     print('Match')
 else:
     print('No match')
+
+if re.search(pattern, "eggspamsausagespam"):
+    print("Match")
+else:
+    print("No match")
+
+print(re.findall(pattern, "eggspamsausagespam"))
+
+match = re.search(pattern, "eggspamsausage")
+if match:
+    print(match.group())
+    print(match.start())
+    print(match.end())
+    print(match.span())
